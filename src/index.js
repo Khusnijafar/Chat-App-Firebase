2600fb8de25a8eb91afa1306c5decb80896755ed
Khusni Jafar <khusnijafar@gmail.com>
2019-08-20 00:00:30 +0700

    add redux and isloading

--- src/index.js
+++ src/index.js
@@ -1,23 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Spinner from './Spinner'
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import firebase from './firebase'
 
-import 'semantic-ui-css/semantic.min.css'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './reducers';
+import { setUser } from './actions'
 
-const Root = () => (
+const store = createStore(rootReducer, composeWithDevTools())
+
+class Root extends React.Component {
+    componentDidMount() {
+        console.log(this.props.isLoading)
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                // console.log(user)
+                this.props.setUser(user)
+                this.props.history.push('/')
+            }
+        })
+    }
+
+    render() {
+       return this.props.isLoading ? <Spinner /> : (
+                <Switch>
+                    <Route exact path='/' component={App}/>
+                    <Route path='/login' component={Login}/>
+                    <Route path='/register' component={Register}/>
+                </Switch>
+        )        
+    }
+}
+
+const mapStateFromProps = state => ({
+    isLoading: state.user.isLoading
+})
+
+const RootWithAuth = withRouter(connect(mapStateFromProps, { setUser })(Root))
+ 
+ReactDOM.render(
+<Provider store={store}>
     <Router>
-        <Switch>
-            <Route exact path='/' component={App}/>
-            <Route path='/login' component={Login}/>
-            <Route path='/register' component={Register}/>
-        </Switch>
+        <RootWithAuth />
     </Router>
-)
-
-ReactDOM.render(<Root />, document.getElementById('root'));
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
 
